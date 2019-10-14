12ed45d0f3135cd04d8d0ad730dc193b205df32e
yangzhongxu <yangzhongxu@okay.cn>
2019-10-14 16:20:23 +0800

    add first util

--- app/src/main/java/yzx/app/image/design/ui/Common.kt
+++ app/src/main/java/yzx/app/image/design/ui/Common.kt
@@ -4,9 +4,10 @@ import android.Manifest
 import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.Color
-import android.os.Environment
 import com.blankj.utilcode.util.FileIOUtils
+import com.blankj.utilcode.util.PathUtils
 import yzx.app.image.design.utils.application
+import yzx.app.image.design.utils.longToast
 import yzx.app.image.design.utils.toast
 import yzx.app.image.permissionutil.PermissionRequester
 import java.io.ByteArrayOutputStream
@@ -17,7 +18,7 @@ interface IImageDesignActivity
 
 
 val cacheDir = File(application.filesDir, "BMP_CACHE")
-val saveDir = File(Environment.getExternalStorageDirectory(), "ImageXX")
+val saveDir = File(PathUtils.getExternalPicturesPath(), "ImageXO")
 
 
 fun IImageDesignActivity.makeStatusBar() {
@@ -29,11 +30,11 @@ fun IImageDesignActivity.startSaveBitmap(bitmap: Bitmap) {
     PermissionRequester.request(this as Activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) { result ->
         if (result) {
             saveDir.mkdir()
-            val targetFile = File(saveDir, "ImageXX_${System.currentTimeMillis()}.png")
+            val targetFile = File(saveDir, "ImageXO_${System.currentTimeMillis()}.png")
             val out = ByteArrayOutputStream()
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
             FileIOUtils.writeFileFromBytesByStream(targetFile, out.toByteArray())
-            toast("已保存到相册")
+            longToast("已保存到系统相册ImageXO文件夹中")
             finish()
         } else {
             toast("未授予存储权限")
