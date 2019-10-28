a80e421350279ca42d876e3d29b01434eb2f9a55
yangzhongxu <yangzhongxu@okay.cn>
2019-10-28 18:45:59 +0800

    add http

--- libhttp/src/main/java/yzx/app/image/libhttp/UploadFileInfo.kt
+++ libhttp/src/main/java/yzx/app/image/libhttp/UploadFileInfo.kt
@@ -5,8 +5,18 @@ import java.io.File
 class UploadFileInfo {
 
     var key: String? = null
+
     var fileName: String? = null
+
     var file: File? = null
+        set(value) {
+            field = value
+            bytes?.run { throw IllegalStateException("bytes is not null") }
+        }
     var bytes: ByteArray? = null
+        set(value) {
+            field = value
+            file?.run { throw IllegalStateException("file is not null") }
+        }
 
 }
